ccb47b21e1f2ce060b1fd7da6f006503a9320dd7
Mouhcine Daali <the.daali.mouhcine@gmail.com>
2023-10-12 16:56:53 +0200

    complete the post ui

--- src/components/post/PostComponent.vue
+++ src/components/post/PostComponent.vue
@@ -1,21 +1,31 @@
 <template>
-  <div class="w-3/5">
+  <div class="w-3/5 space-y-5 px-10 pt-5 pb-10 shadow-md cursor-pointer hover:scale-105 transition-transform duration-300 ease-linear">
     <div class="w-full flex justify-between">
-      <div class="flex gap-x-3">
-        <img :src="post.profile.img" class="w-8 h-8" />
-        <div class="flex flex-col">
-          <h3 class="w-fit">
+      <div class="flex gap-x-3 items-center">
+        <img :src="post.profile.img" class="w-8 h-8 rounded-sm" />
+        <div class="flex flex-col -space-y-2">
+          <h3 class="w-fit font-semibold">
             {{ post.profile.fullName }}
           </h3>
-          <h5>
+          <h5 class="text-xs">
             {{ post.profile.userName }}
           </h5>
         </div>
       </div>
+      <div class="flex items-center">
+        <TimeIcon />
+        <p class="text-xs">10h</p>
+      </div>
+    </div>
+    <div class="w-full">
+      <p>
+        {{ post.content }}
+      </p>
     </div>
   </div>
 </template>
 
 <script setup>
+import TimeIcon from "../../icon/TimeIcon.vue";
 const props = defineProps(["post"]);
 </script>
