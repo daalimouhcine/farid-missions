d1b0ade10e697aec35112340d2351e9c69a4b553
Mouhcine Daali <the.daali.mouhcine@gmail.com>
2023-10-17 22:06:49 +0200

    complete first version of the cart design and partitions and add on scroll changes

--- src/components/scrollTask/Card.vue
+++ src/components/scrollTask/Card.vue
@@ -1,36 +1,68 @@
 <template>
-  <div class="flex gap-x-5 w-full mx-5 border-y px-5">
-    <img
-      src="https://images.unsplash.com/photo-1633332755192-727a05c4013d"
-      class="w-12 h-12 rounded-sm"
-    />
-    <div class="space-y-3">
-      <div class="flex gap-x-5 justify-between">
+  <!-- class="relative flex justify-center items-center max-sm:gap-x-0 gap-x-5 w-full border-y" -->
+  <div
+    :class="{
+      'sticky top-0 flex justify-center items-center max-sm:gap-x-0 gap-x-5 w-full border-y': true,
+    }"
+  >
+    <div
+      :class="{
+        '-translate-x-12': isScrolled,
+        'w-1/12 max-sm:-translate-x-16 max-md:-translate-x-20 -translate-x-24':true}"
+    >
+      <img
+        src="https://images.unsplash.com/photo-1633332755192-727a05c4013d"
+        :class="{
+            'w-16 h-16 my-5': isScrolled,
+             'max-sm:w-20 max-md:w-28 w-36 max-w-max rounded-md': true }"
+      />
+    </div>
+    <div
+      :class="{
+        'max-md:mt-0 mt-0': isScrolled,
+        'w-full space-y-3 max-md:mt-7 mt-10': true,
+      }"
+    >
+      <div class="flex gap-x-5 justify-between items-center">
         <div class="flex flex-col -space-y-2">
-          <h3 class="w-fit font-semibold">John Tyson Jr.</h3>
+          <h3 class="w-fit max-sm:text-base text-lg font-serif font-semibold">
+            John Tyson Jr.
+          </h3>
           <h5 class="text-xs">@johntysonjr</h5>
         </div>
         <button
           type="submit"
-          class="flex gap-x-1 items-center bg-amber-800 hover:bg-amber-700 transition-colors text-gray-100 font-semibold py-1.5 px-1 sm:px-2 rounded-lg"
+          class="h-fit flex gap-x-1 font-serif items-center max-sm:text-xs text-sm bg-amber-800 hover:bg-amber-700 transition-colors text-gray-100 font-semibold py-1.5 px-5 sm:px-7 rounded-lg"
         >
-          <p class="text-xs">Follow</p>
+          Follow
         </button>
       </div>
-      <div class="w-full">
-        <p>
+      <div v-if="!isScrolled" class="w-full">
+        <p class="max-sm:text-xs max-md:text-sm">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
           voluptatum, voluptate, quibusdam, quia voluptas quod quos
           exercitationem voluptatem quas quibusdam, quia voluptas quod quos
           exercitationem voluptatem quas.
         </p>
       </div>
-      <div class="w-full flex">
-        <button class="border-r appearance-none">1.5K ********</button>
-        <button class="border-r appearance-none">150 ********</button>
-        <button class="border-r appearance-none">1K ********</button>
-        <button class="appearance-none">15 ********</button>
+      <div v-if="!isScrolled" class="w-full flex h-14">
+        <button class="flex-1 border-r appearance-none">1.5K ********</button>
+        <button class="flex-1 border-r appearance-none">150 ********</button>
+        <button class="flex-1 border-r appearance-none">1K ********</button>
+        <button class="flex-1 appearance-none">15 ********</button>
       </div>
     </div>
   </div>
 </template>
+
+<script setup>
+import { ref, onMounted } from "vue";
+
+const isScrolled = ref(false);
+
+onMounted(() => {
+  window.addEventListener("scroll", () => {
+    isScrolled.value = window.scrollY > 0;
+  });
+});
+</script>
--- src/views/ScrollTaskView.vue
+++ src/views/ScrollTaskView.vue
@@ -1,18 +1,22 @@
 <template>
   <div class="flex gap-x-5">
-    <div class="max-md:w-2/12 w-1/12 max-sm:hidden h-screen bg-blue-200">
+    <div class="fixed left-0 w-1/12 max-sm:hidden h-screen bg-blue-200">
       <!-- Left content -->
     </div>
     <div
-      class="max-sm:w-10/12 max-md:w-9/12 w-6/12 relative bg-red-200 ml-auto mr-5 border-x-2 border-black overflow-y-scroll"
+      class="max-sm:w-10/12 max-md:w-9/12 w-6/12 bg-red-200 max-md:mr-0 max-md:-translate-x-2 mx-auto -translate-x-[5vw] border-x-2 px-5 border-black"
     >
-      <div class="absolute w-full h-[200vh] bg-gray-300 -ml-10">
+      <div class="w-full h-[200vh] my-5 ">
         <!-- the scrollable content goes here -->
-        <!-- <div class="w-full -ml-10 h-[20vh] bg-gray-300"></div> -->
+        <Card />
       </div>
     </div>
-    <div class="w-3/12 max-md:hidden h-screen bg-green-200">
+    <div class="fixed right-0 w-3/12 max-md:hidden h-screen bg-green-200">
       <!-- Right content -->
     </div>
   </div>
 </template>
+
+<script setup>
+import Card from "../components/scrollTask/Card.vue";
+</script>
