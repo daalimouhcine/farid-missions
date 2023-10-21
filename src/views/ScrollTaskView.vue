ec4836d8592ab72e7fb088d475790fbb1ffe6359
Mouhcine Daali <the.daali.mouhcine@gmail.com>
2023-10-21 14:31:13 +0200

    remove console text

--- src/views/ScrollTaskView.vue
+++ src/views/ScrollTaskView.vue
@@ -2,7 +2,6 @@
   <div class="flex gap-x-5">
     <div class="fixed left-0 w-1/12 max-sm:hidden h-screen bg-blue-200">
       <!-- Left content -->
-      {{ currentItem }} dsdf
     </div>
     <div
       class="max-sm:w-10/12 max-md:w-9/12 w-6/12 bg-gray-100 max-md:mr-0 max-md:-translate-x-2 mx-auto -translate-x-[5vw] border-x-[4px] px-5 border-gray-300"
