d99dacb5ccfe0b5241fe760bd5c2f75b90f19ec4
Mouhcine Daali <the.daali.mouhcine@gmail.com>
2023-10-22 20:16:39 +0200

    make the scroll and icon event based on chapter and make it follow the scroll

--- src/views/ScrollTaskView.vue
+++ src/views/ScrollTaskView.vue
@@ -10,34 +10,39 @@
         <Card />
         <!-- the scrollable content goes here -->
         <div
-          v-for="(_, idx) in Array(10).fill(0)"
-          :key="idx"
-          @click="openModal(idx)"
+          v-for="(item, itemIdx) in items"
+          :key="itemIdx"
           class="relative w-full space-y-5 px-3 md:px-10 pt-3 md:pt-5 pb-5 md:pb-10 shadow-md cursor-pointer"
         >
           <div
             class="w-1 rounded-full h-full absolute top-0 left-0 -translate-x-6"
             :class="{
-              'bg-[#711112]': currentItem == idx,
-              'bg-[#CCADAF]': currentItem != idx,
+              'bg-[#711112]': currentItem == itemIdx,
+              'bg-[#CCADAF]': currentItem != itemIdx,
             }"
           >
             <div
-              v-if="currentItem == idx"
-              class="w-fit h-fit absolute -top-4 -translate-x-1/2 ml-0.5"
+              v-if="currentItem == itemIdx"
+              class="w-fit h-fit sticky top-52 -translate-y-full -translate-x-1/2 flex flex-col gap-y-0.5 items-center ml-0.5"
             >
+              <button class="w-fit px-2 bg-[#711112] text-white rounded-md">
+                {{ item.chapterNumber }}
+              </button>
               <OpenBookIcon />
             </div>
             <div
               v-else
-              class="w-fit h-fit absolute -top-7 -translate-x-1/2 ml-[2px]"
+              class="w-fit h-fit fixed -top-7 -translate-x-1/2 ml-[2px]"
             >
               <CloseBookIcon />
             </div>
           </div>
-          <div class="item h-40 w-full bg-zinc-600" :id="idx">
-            <!-- <PostComponent :post="post" /> -->
-          </div>
+          <div
+            v-for="(element, idx) in item.elements"
+            :key="idx"
+            class="item h-40 w-full bg-zinc-600"
+            :id="itemIdx"
+          ></div>
         </div>
       </div>
     </div>
@@ -54,6 +59,49 @@ import OpenBookIcon from "../icons/OpenBookIcon.vue";
 import CloseBookIcon from "../icons/CloseBookIcon.vue";
 
 const currentItem = ref("");
+
+const items = [
+  {
+    chapterNumber: "I",
+    elements: Array(3).fill(0),
+  },
+  {
+    chapterNumber: "II",
+    elements: Array(5).fill(0),
+  },
+  {
+    chapterNumber: "III",
+    elements: Array(2).fill(0),
+  },
+  {
+    chapterNumber: "IV",
+    elements: Array(4).fill(0),
+  },
+  {
+    chapterNumber: "V",
+    elements: Array(6).fill(0),
+  },
+  {
+    chapterNumber: "VI",
+    elements: Array(3).fill(0),
+  },
+  {
+    chapterNumber: "VII",
+    elements: Array(5).fill(0),
+  },
+  {
+    chapterNumber: "VIII",
+    elements: Array(2).fill(0),
+  },
+  {
+    chapterNumber: "IX",
+    elements: Array(4).fill(0),
+  },
+  {
+    chapterNumber: "X",
+    elements: Array(6).fill(0),
+  },
+];
 
 onMounted(() => {
   const items = document.querySelectorAll(".item");
